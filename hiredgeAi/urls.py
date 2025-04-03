709bfc7685bb9b8324c066f3e8831cda1739e333
MrCrowNFT <mrcrownft@gmail.com>
2025-04-03 07:18:28 -0300

    urls.py

--- hiredgeAi/urls.py
+++ hiredgeAi/urls.py
@@ -16,11 +16,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-   #url to connect to the app, bascally, when going to empty string, 
-   # we forward the request to apps url
-    path("", include("resume_app.urls"))
+    path('', include('resume_app.urls')),
+   
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
